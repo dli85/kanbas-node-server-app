5a7e45dc055ee55c2f116c560ee29e3ba147a64c
David Li <dali7758769@gmail.com>
2024-04-02 22:45:28 -0400

    init

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-import express from 'express'
-import Hello from "./hello.js"
+import express from "express";
+import Hello from "./hello.js";
+import cors from "cors";
 import Lab5 from "./Lab5.js";
-const app = express()
-Hello(app)
+import CourseRoutes from "./Kanbas/courses/Routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+//
+const app = express();
+app.use(cors());
+Hello(app);
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
-app.listen(4000)
+//app.listen(4000);
+app.listen(process.env.PORT || 4000);
