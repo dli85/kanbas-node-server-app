ca042d5639d5694643bcd3e5478da01aeb73e842
David Li <dali7758769@gmail.com>
2024-04-03 15:35:27 -0400

    try

--- App.js
+++ App.js
@@ -31,15 +31,15 @@ app.use(
 //   }
 // });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // app.options("*", (req, res) => {
 //   res.header("Access-Control-Allow-Origin", req.headers.origin);
