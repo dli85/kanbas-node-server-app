4a2442d44e3d0b764f4a6f2a4082b2d470259db3
David Li <dali7758769@gmail.com>
2024-04-02 23:28:47 -0400

    login fix

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ import CourseRoutes from "./Kanbas/courses/Routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 //
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+  })
+);
 Hello(app);
 app.use(express.json());
 CourseRoutes(app);
