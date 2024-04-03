384d688e5a0db71457cc9214620bf678f2925d13
David Li <dali7758769@gmail.com>
2024-04-03 01:16:23 -0400

    try again

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/Routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-//
 const app = express();
 
 app.use(
@@ -32,15 +31,15 @@ app.use(
 //   }
 // });
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // app.options("*", (req, res) => {
 //   res.header("Access-Control-Allow-Origin", req.headers.origin);
