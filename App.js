6073fe259bfb910a19f8d4cb3adb364b1cf74e75
David Li <dali7758769@gmail.com>
2024-04-03 00:37:33 -0400

    try again

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ app.use(
 // });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", true);
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
