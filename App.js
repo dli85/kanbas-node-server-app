e96263b9e60b09cd6869ee053d49f996e2e124d3
David Li <dali7758769@gmail.com>
2024-04-03 00:39:36 -0400

    try again

--- App.js
+++ App.js
@@ -30,7 +30,10 @@ app.use(
 // });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", true);
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://a5--celebrated-pasca-32173b.netlify.app"
+  );
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
