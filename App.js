ed7c9d8b1051e7183c2584dc80d34dabf500173f
David Li <dali7758769@gmail.com>
2024-04-03 00:22:38 -0400

    not gonna work

--- App.js
+++ App.js
@@ -13,20 +13,20 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-  if ("OPTIONS" === req.method) {
-    res.sendStatus(200);
-  } else {
-    console.log(`${req.ip} ${req.method} ${req.url}`);
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+//   if ("OPTIONS" === req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     console.log(`${req.ip} ${req.method} ${req.url}`);
+//     next();
+//   }
+// });
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", req.headers.origin);
